98a68626a034332799165a618a59c6b9f4864ec9
filinoffigor-art <filinoff.igor@gmail.com>
2026-09-12 12:21:59 +0300

    Add files via upload
    Add Teplaya Company 4.0 alpha1

---
+++ tk4/android/app/src/main/assets/config.js
@@ -0,0 +1,5 @@
+window.TK4_CONFIG={
+  version:"4.0.0-alpha1",
+  mode:"demo",
+  apiUrl:""
+};
---
+++ tk4/android/app/src/main/java/ru/teplayakompaniya/tk4/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.teplayakompaniya.tk4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
